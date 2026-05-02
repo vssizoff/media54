4e3efded29d9383c08fdd89b80d1d8eb981eff8c
sizoff <sizoff.dev@yandex.ru>
2026-05-02 14:48:11 +0300

    mini fix

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -8,12 +8,6 @@ import {router} from "@renderer/router.js";
 import {definePreset} from "@primevue/themes";
 import ConfirmationService from 'primevue/confirmationservice';
 
-import {
-  GlobalWorkerOptions,
-} from 'vue-pdf-embed/dist/index.essential.mjs'
-import PdfWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?url'
-GlobalWorkerOptions.workerSrc = PdfWorker
-
 const app = createApp(App);
 
 app.use(PrimeVue, {
